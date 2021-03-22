727ff196fde12f24963b80fa000c7754d161cd28
chrek <chrek@ymail.com>
2021-03-22 17:18:44 +0100

    first commit

---
+++ src/freestyle/HelloJenkins.java
@@ -0,0 +1,13 @@
+package freestyle;
+
+/**
+ * Hello world!
+ *
+ */
+public class HelloJenkins 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World From Jenkins!" );
+    }
+}
